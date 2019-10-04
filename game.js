a1be77a546f1cc85fb28dc0ac5185cf34d748e05
Eren Cetinbas <eren.bilal1510@googlemail.com>
2019-10-04 15:44:55 +0200

    Add function

--- game.js
+++ game.js
@@ -7,19 +7,32 @@ function setupCanvas() {
   canvas.height = 400;
   ctx.scale(10, 10);
 
-//  background
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+ background();
+  
 
-//  right wall
-  ctx.fillStyle = "white";
-  ctx.fillRect(30,0,2,34);
+ rightWall();
+  
+ leftWall();
 
-//  left wall
-ctx.fillStyle ="white";
-ctx.fillRect(8,0,2,34);
 
-// paddle
-ctx.fillStyle ="white";
-ctx.fillRect(19,34,2,7);
+ paddle();
+}
+
+
+function background() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+function rightWall() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(30,0,2,34);
+
+}
+function leftWall() {
+    ctx.fillStyle ="white";
+    ctx.fillRect(8,0,2,34);
+}
+function paddle(){
+    ctx.fillStyle ="white";
+    ctx.fillRect(19,34,2,7);
 }
